bfc960e50fca89f180f59854515575cab89ac9e9
Cristhian Caldas <c.caldas.m@gmail.com>
2022-02-10 00:46:16 -0500

    subiendo cambios

--- service-tarifario/src/main/resources/data.sql
+++ service-tarifario/src/main/resources/data.sql
@@ -12,7 +12,20 @@ CREATE TABLE tarifario (
 );
 
 INSERT INTO tarifario (id, codigo, origen, destino, factor) VALUES
-  (1, 'USDPEN', 'USD', 'PEN', 3.8001),
-  (2, 'PENUSD', 'PEN', 'USD', 3.9815);
-  
+  (1, 'USDPEN', 'USD', 'PEN', 3.8091),
+  (2, 'USDEUR', 'USD', 'EUR', 3.7459),
+  (3, 'USDJPY', 'USD', 'JPY', 2.3679);
+
+ /* (4, 'PENUSD', 'PEN', 'USD', 3.2569),
+  (5, 'PENEUR', 'PEN', 'EUR', 3.9819),
+  (6, 'PENJPY', 'PEN', 'JPY', 3.7599),
+
+  (7, 'EURPEN', 'EUR', 'PEN', 2.6549),
+  (8, 'EURUSD', 'EUR', 'USD', 4.2549),
+  (9, 'EURJPY', 'EUR', 'JPY', 5.2459),
+
+  (10, 'JPYPEN', 'JPY', 'PEN', 2.6579),
+  (11, 'JPYUSD', 'JPY', 'USD', 4.2559),
+  (12, 'JPYEUR', 'JPY', 'EUR', 5.2479);*/
+
 SELECT * FROM tarifario;
